067dae97dc53ad3b354dbcf3c3a22488f543b35f
Ayesh1998 <ayeshlak1998@gmail.com>
2020-03-29 11:57:49 +0530

    Implemented videoItem Component and added some styles

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/header";
 import BottomTabBar from "./components/bottom-tab";
+import VideoItem from "./components/video-item";
+import data from "./data.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <View style={styles.body}></View>
+      <View style={styles.body}>
+        <VideoItem video={data.items[0]} />
+      </View>
+
       <BottomTabBar />
     </View>
   );
